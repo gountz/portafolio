3905de7aed16bcc06034e8fd0986daf5f30035ff
gountz <76885512+gountz@users.noreply.github.com>
2021-06-16 08:04:52 -0300

    Delete proyect.js

--- proyect.js
+++
@@ -1,7 +0,0 @@
-function desplegable($element){
-    $element.parentElement.classList.toggle('fondoProyecto');
-    $element.parentElement.getElementsByTagName('a')[0].classList.toggle('acceso-toggle')
-    $element.parentElement.getElementsByTagName('i')[0].classList.toggle('bxs-chevron-right-square');
-    $element.parentElement.getElementsByTagName('i')[0].classList.toggle('bxs-chevron-down-square');
-    
-}
